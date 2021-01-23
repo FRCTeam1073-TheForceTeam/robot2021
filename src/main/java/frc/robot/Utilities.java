83427df9f90135630e9698530d8ae6a2d0d5e1e9
BenWertzDaHuman <benwertz216@gmail.com>
2021-01-23 13:09:34 -0500

    Created skeleton for magazine subsystem, added Utilities.java
    The magazine subsystem as of now should be able to run the actual mechanism. There aren't controls yet, though. The Utilities.java file is for useful functions that we'll use throughout the code. There's only one in there right now, for clipping values between a minimum and maximum value, but as we need to use other functions (like finding the distance between two angles, interpolating between values, etc.), we can add them in.

---
+++ src/main/java/frc/robot/Utilities.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+public class Utilities {
+
+    /**Clips values to [min,max] */
+    public static double clipRange(double value, double min, double max) {
+        return Math.min(max, Math.max(min, value));
+    }
+
+    /**Clips values to [0,1] */
+    public static double clipRange(double value) {
+        return clipRange(value, 0, 1);
+    }
+
+    /**Clips values to [-dist,dist] */
+    public static double clipRange(double value, double dist) {
+        return clipRange(value, -dist, dist);
+    }
+}
--- src/main/java/frc/robot/subsystems/Magazine.java
+++ src/main/java/frc/robot/subsystems/Magazine.java
@@ -1,16 +1,47 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Utilities;
 
 public class Magazine extends SubsystemBase {
     
+    private WPI_TalonSRX magazineMotor;
+    private int cellCount;
+    private double P = 0;
+    private double I = 0;
+    private double D = 0;
+    private double F = 0;
+    private boolean cellCheck, cellEntering, cellExiting;
+  
     public Magazine() {
-
+      magazineMotor = new WPI_TalonSRX(26);
+      cellCount = 0;
+      cellCheck = false;
+      cellEntering = false;
+      cellExiting = false;
+      magazineMotor.configFactoryDefault();
+      magazineMotor.setSafetyEnabled(false);
+      magazineMotor.setNeutralMode(NeutralMode.Brake);
+      magazineMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+      // magazineMotor.config_kP(0, P);
+      // magazineMotor.config_kI(0, I);
+      // magazineMotor.config_kD(0, D);
+      // magazineMotor.config_kF(0, F);
     }
 
     /// Set the induct forward/reverse velocity.
     public void setVelocity(double inductVelocity) {
 
+    }
+
+    /// Set the induct forward/reverse velocity.
+    public void setPower(double power) {
+      magazineMotor.set(ControlMode.PercentOutput, Utilities.clipRange(power, 1));
     }
 
     /// Return load on the magazine motor.
@@ -20,28 +51,28 @@ public class Magazine extends SubsystemBase {
 
     /// Return the accumulated position of the magazine.
     public double getPosition() {
-      return 0.0;
+      return magazineMotor.getSelectedSensorPosition(0);
     }
 
     /// Return the current power cell count.
     public int getPowerCellCount() {
-      return 0;
+      return cellCount;
     }
 
 
     // Add to the number of power cells.
     public void addPowerCell() {
-
+      cellCount++;
     }
     
     // Reduce the number of power cells.
     public void removePowerCell() {
-
+      cellCount--;
     }
 
     // Reset number of power cells to zero.
     public void resetPowerCellCount() {
-    
+      cellCount=0;    
     }
 
     
