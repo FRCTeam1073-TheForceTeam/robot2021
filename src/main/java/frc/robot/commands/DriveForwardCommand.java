2c41795e9d68c1628063218a71e7487b0e3bf468
Chris-Chan <55216556+ChristianW1073@users.noreply.github.com>
2021-01-28 14:08:02 -0500

    Added an print statement for distance traveled
    I will use it to see how much the robot thinks it overshoots (if values seem off, I'll try to take measurements and compare)

--- src/main/java/frc/robot/commands/DriveForwardCommand.java
+++ src/main/java/frc/robot/commands/DriveForwardCommand.java
@@ -72,6 +72,7 @@ public class DriveForwardCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    System.out.println(currentPose.getTranslation().getDistance(initPose.getTranslation()));
   }
 
   // Returns true when the command should end.
