bf84cf68f5407e6c0ba67390a7844570758cde65
Chris-Chan <55216556+ChristianW1073@users.noreply.github.com>
2021-01-30 10:50:34 -0800

    auto formatted and removed clip functions

--- src/main/java/frc/robot/Utility.java
+++ src/main/java/frc/robot/Utility.java
@@ -3,6 +3,7 @@ package frc.robot;
 public class Utility {
     /**
      * sets raw axis value inside the deadzone to zero
+     * 
      * @param rawAxisValue
      * @return deadzoned axisValue
      */
@@ -16,20 +17,5 @@ public class Utility {
 
     public static double deadzone(double rawAxisValue) {
         return deadzone(rawAxisValue, Constants.CONTROLLER_DEADZONE);
-    }
-
-    /**Clips values to [min,max] */
-    public static double clipRange(double value, double min, double max) {
-        return Math.min(max, Math.max(min, value));
-    }
-
-    /**Clips values to [0,1] */
-    public static double clipRange(double value) {
-        return clipRange(value, 0, 1);
-    }
-
-    /**Clips values to [-dist,dist] */
-    public static double clipRange(double value, double dist) {
-        return clipRange(value, -dist, dist);
     }
 }
--- src/main/java/frc/robot/subsystems/Magazine.java
+++ src/main/java/frc/robot/subsystems/Magazine.java
@@ -8,86 +8,86 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpiutil.math.MathUtil;
 import frc.robot.Utility;
 
 public class Magazine extends SubsystemBase {
-    
-    private WPI_TalonSRX magazineMotor;
-    private int cellCount;
-    private double P = 0;
-    private double I = 0;
-    private double D = 0;
-    private double F = 0;
-    private boolean cellCheck, cellEntering, cellExiting;
-  
-    public Magazine() {
-      magazineMotor = new WPI_TalonSRX(26);
-      cellCount = 0;
-      cellCheck = false;
-      cellEntering = false;
-      cellExiting = false;
-      magazineMotor.configFactoryDefault();
-      magazineMotor.setSafetyEnabled(false);
-      magazineMotor.setNeutralMode(NeutralMode.Brake);
-      ErrorCode magazineEncoderAttached = magazineMotor
-          .configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-      if (magazineEncoderAttached.value != 0) {
-        System.out.println("[Magazine] Error: Magazine motor (CAN ID 26) cannot find encoder (Error code: " + magazineEncoderAttached.value + ").");
-      }
-      magazineMotor.config_kP(0, P);
-      magazineMotor.config_kI(0, I);
-      magazineMotor.config_kD(0, D);
-      magazineMotor.config_kF(0, F);
-    }
 
-    /// Set the induct forward/reverse velocity.
-    public void setVelocity(double inductVelocity) {
-      magazineMotor.set(ControlMode.Velocity, inductVelocity);
-    }
+  private WPI_TalonSRX magazineMotor;
+  private int cellCount;
+  private double P = 0;
+  private double I = 0;
+  private double D = 0;
+  private double F = 0;
+  private boolean cellCheck, cellEntering, cellExiting;
 
-    /// Set the induct forward/reverse velocity.
-    public void setPower(double power) {
-      magazineMotor.set(ControlMode.PercentOutput, Utility.clipRange(power, 1));
+  public Magazine() {
+    magazineMotor = new WPI_TalonSRX(26);
+    cellCount = 0;
+    cellCheck = false;
+    cellEntering = false;
+    cellExiting = false;
+    magazineMotor.configFactoryDefault();
+    magazineMotor.setSafetyEnabled(false);
+    magazineMotor.setNeutralMode(NeutralMode.Brake);
+    ErrorCode magazineEncoderAttached = magazineMotor
+        .configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+    if (magazineEncoderAttached.value != 0) {
+      System.out.println("[Magazine] Error: Magazine motor (CAN ID 26) cannot find encoder (Error code: "
+          + magazineEncoderAttached.value + ").");
     }
+    magazineMotor.config_kP(0, P);
+    magazineMotor.config_kI(0, I);
+    magazineMotor.config_kD(0, D);
+    magazineMotor.config_kF(0, F);
+  }
 
-    /// Return load on the magazine motor.
-    public double getLoad() {
-      return 0.0;
-    }
+  /// Set the induct forward/reverse velocity.
+  public void setVelocity(double inductVelocity) {
+    magazineMotor.set(ControlMode.Velocity, inductVelocity);
+  }
 
-    /// Return the accumulated position of the magazine.
-    public double getPosition() {
-      return magazineMotor.getSelectedSensorPosition(0);
-    }
+  /// Set the induct forward/reverse velocity.
+  public void setPower(double power) {
+    magazineMotor.set(ControlMode.PercentOutput, MathUtil.clamp(power, -1, 1));
+  }
 
-    /// Return the current power cell count.
-    public int getPowerCellCount() {
-      return cellCount;
-    }
+  /// Return load on the magazine motor.
+  public double getLoad() {
+    return 0.0;
+  }
 
+  /// Return the accumulated position of the magazine.
+  public double getPosition() {
+    return magazineMotor.getSelectedSensorPosition(0);
+  }
 
-    // Add to the number of power cells.
-    public void addPowerCell() {
-      cellCount++;
-    }
-    
-    // Reduce the number of power cells.
-    public void removePowerCell() {
-      cellCount--;
-    }
+  /// Return the current power cell count.
+  public int getPowerCellCount() {
+    return cellCount;
+  }
 
-    // Reset number of power cells to zero.
-    public void resetPowerCellCount() {
-      cellCount = 0;
-    }
+  // Add to the number of power cells.
+  public void addPowerCell() {
+    cellCount++;
+  }
 
-    
-    @Override
-    public void periodic() {
-      SmartDashboard.putNumber("Magazine Velocity", magazineMotor.getSelectedSensorVelocity());
-      SmartDashboard.putNumber("Magazine Position", magazineMotor.getSelectedSensorPosition() * 679.0);
-      if (OI.operatorController.getAButtonPressed()) {
-        magazineMotor.setSelectedSensorPosition(0);
-      }
+  // Reduce the number of power cells.
+  public void removePowerCell() {
+    cellCount--;
+  }
+
+  // Reset number of power cells to zero.
+  public void resetPowerCellCount() {
+    cellCount = 0;
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Magazine Velocity", magazineMotor.getSelectedSensorVelocity());
+    SmartDashboard.putNumber("Magazine Position", magazineMotor.getSelectedSensorPosition() * 679.0);
+    if (OI.operatorController.getAButtonPressed()) {
+      magazineMotor.setSelectedSensorPosition(0);
     }
+  }
 }
