5d975f39419c3720edc48b1d8a7f7f0662325d56
Chris-Chan <55216556+ChristianW1073@users.noreply.github.com>
2021-02-23 15:12:02 -0800

    saving

---
+++ src/main/java/frc/robot/Shuffleboard.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import java.util.Map;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shuffleboard extends SubsystemBase {
+    ShuffleboardTab tab;
+    /*
+     * private NetworkTableEntry P_testing; private NetworkTableEntry I_testing;
+     * private NetworkTableEntry D_testing; double P_Value = 0.0; double I_Value =
+     * 0.0; double D_Value = 0.0;
+     */
+
+    public Shuffleboard() {
+    }
+
+    @Override
+    public void periodic() {
+    }
+}
