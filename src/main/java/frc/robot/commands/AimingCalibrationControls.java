bce4bf0ad064d5eb6c3fbda3f1307885a051c116
Ben Wertz <benwertz216@gmail.com>
2021-02-27 14:16:21 -0500

    Yeah that

--- src/main/java/frc/robot/commands/AimingCalibrationControls.java
+++ src/main/java/frc/robot/commands/AimingCalibrationControls.java
@@ -4,8 +4,6 @@
 
 package frc.robot.commands;
 
-import com.ctre.phoenix.Util;
-
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
