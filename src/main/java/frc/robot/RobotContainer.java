3d32ac09cfeab0e85464d9df58d9c5cbdfc34700
Ben Wertz <benwertz216@gmail.com>
2021-07-19 20:28:16 -0400

    Maybe it's okay?
    I might revert this commit if I find a better solution (Evan wants it to stay granular and wants the throttle to interrupt the auto-firing to prevent issues, which might or might not be doable without horrific jankiness).

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -133,6 +133,7 @@ public class RobotContainer {
     turret.setDefaultCommand(teleTurret);
     collector.setDefaultCommand(teleCollect);
     shooter.setDefaultCommand(teleShooter);
+    magazine.setDefaultCommand(teleMagazine);
   }
 
   /**
@@ -177,14 +178,14 @@ public class RobotContainer {
     // new AdvanceMagazineCommand(magazine, 1.25, 4)
     // )
     );
-    (new JoystickButton(OI.operatorController, XboxController.Button.kBumperLeft.value))
-        .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, 1));
-    (new JoystickButton(OI.operatorController, XboxController.Button.kBumperRight.value))
-        .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, -0.1));
-    (new JoystickButton(OI.driverController, XboxController.Button.kY.value))
-        .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, 1));
-    (new JoystickButton(OI.driverController, XboxController.Button.kA.value))
-        .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, -0.1));
+    // (new JoystickButton(OI.operatorController, XboxController.Button.kBumperLeft.value))
+    //     .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, 1));
+    // (new JoystickButton(OI.operatorController, XboxController.Button.kBumperRight.value))
+    //     .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, -0.1));
+    // (new JoystickButton(OI.driverController, XboxController.Button.kY.value))
+    //     .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, 1));
+    // (new JoystickButton(OI.driverController, XboxController.Button.kA.value))
+    //     .whenPressed(new AdvanceMagazineCommand(magazine, 1.25, -0.1));
   }
 
   /**
--- src/main/java/frc/robot/commands/MagazineControls.java
+++ src/main/java/frc/robot/commands/MagazineControls.java
@@ -31,22 +31,19 @@ public class MagazineControls extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    magazineVelocity = OI.operatorController.getRawAxis(1) * Constants.MAX_FLYWHEEL_SPEED;
-    // if (OI.operatorController.getBumper(Hand.kLeft)) {
-    //   magazineVelocity *= -1;
-    // }
-
-    //SmartDashboard.putNumber("Magazine Power", magazineVelocity);
-
+    if (OI.driverController.getAButton() || OI.operatorController.getBumper(Hand.kRight)) {
+      magazineVelocity = -0.1;
+    } else if (OI.driverController.getYButton() || OI.operatorController.getBumper(Hand.kLeft)) {
+      magazineVelocity = 1.25;
+    } else {
+      magazineVelocity = 0;
+    }
     magazine.setVelocity(magazineVelocity);
-
-    // magazine.setVelocity(magazineVelocity);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
   }
 
   // Returns true when the command should end.
