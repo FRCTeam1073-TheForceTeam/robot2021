ba020f5750f09b83aa456e109f1e7f237c212b09
Celia <36650015+classygirl44@users.noreply.github.com>
2021-11-03 19:18:35 -0400

    Revert "Reverting deleting stuff"
    This reverts commit 317ea74e7d72ae6e5565f3a5730ebad49b5adeeb.

--- src/main/java/frc/robot/subsystems/OI.java
+++ src/main/java/frc/robot/subsystems/OI.java
@@ -3,10 +3,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class OI {
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 1eadf2d (Cleared out subsystems and commands folders for rookie training stuff)
     public static XboxController driverController;
     public static XboxController operatorController;
 
@@ -14,8 +10,4 @@ public class OI {
         driverController = new XboxController(0);
         operatorController = new XboxController(1);
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of 1eadf2d (Cleared out subsystems and commands folders for rookie training stuff)
 }
